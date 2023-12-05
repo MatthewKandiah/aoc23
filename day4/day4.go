392b77e871011fb3877401c8dcacc9b27cd7d064
Matthew Kandiah <matthew.kandiah@glean.co>
2023-12-05 20:59:19 +0000

    day4 part1

--- day4/day4.go
+++ day4/day4.go
@@ -1,9 +1,81 @@
 package main
 
 import (
+	"aoc23/util"
 	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"text/scanner"
 )
 
 func main() {
-	fmt.Println("Oof I'm falling behind")
+	lines, err := util.ReadData("day4")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	winnersList, candidatesList, err := parseLines(lines)
+
+	var scores []int
+	for idx := range winnersList {
+		numMatches := countMatches(winnersList[idx], candidatesList[idx])
+		score := int(math.Pow(2, float64(numMatches-1)))
+		scores = append(scores, score)
+	}
+
+	fmt.Println("Solution 1:", util.Sum(scores))
+}
+
+func countMatches(a []int, b []int) int {
+	var result int
+	for _, valA := range a {
+		for _, valB := range b {
+			if valA == valB {
+				result++
+				break
+			}
+		}
+	}
+	return result
+}
+
+func parseLines(lines []string) ([][]int, [][]int, error) {
+	var winnersList [][]int
+	var candidatesList [][]int
+	for _, line := range lines {
+		var winners []int
+		var candidates []int
+		var s scanner.Scanner
+		s.Init(strings.NewReader(line))
+		// skip word Card
+		s.Scan()
+		// skip card number
+		s.Scan()
+		// skip colon
+		s.Scan()
+		// scan winning numbers
+		for s.Scan() == scanner.Int {
+			value, err := strconv.Atoi(s.TokenText())
+			if err != nil {
+				return winnersList, candidatesList, err
+			}
+			winners = append(winners, value)
+		}
+		// skip |
+		// scan candidate numbers
+		for s.Scan() == scanner.Int {
+			value, err := strconv.Atoi(s.TokenText())
+			if err != nil {
+				return winnersList, candidatesList, err
+			}
+			candidates = append(candidates, value)
+		}
+		// end of line
+		winnersList = append(winnersList, winners)
+		candidatesList = append(candidatesList, candidates)
+	}
+	return winnersList, candidatesList, nil
 }
